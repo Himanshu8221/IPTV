fe922d1f8dbcb122be69b6978f5387b741f3ca40
Himanshu Singh <151509258+Himanshu8221@users.noreply.github.com>
2025-06-17 08:56:56 +0530

    Update code.py

--- code.py
+++ code.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # ✅ M3U playlist URL
-M3U_URL = os.getenv("M3U_URL", "http://starshare.org:80/get.php?username=gurmeet&password=gurmeet&type=m3u_plus&output=mpegts")
+M3U_URL = os.getenv("M3U_URL")
 OUTPUT_FILE = "list.m3u"
 HTML_FILE = "index.html"
 COMMIT_MESSAGE = "Update Hindi-only filtered M3U and preview"
