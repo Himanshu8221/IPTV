b2f7d01ba94af4cf55466272c2eeb576aeffbb14
Himanshu Singh <151509258+Himanshu8221@users.noreply.github.com>
2025-06-19 18:40:43 +0530

    Update code.py

--- code.py
+++ code.py
@@ -13,10 +13,10 @@ M3U_URL = os.getenv("M3U_URL")
 # Define categories and channel names
 CATEGORIES = {
     "Entertainment": [
-        "Star Plus", "Star Plus HD", "Star Bharat", "Star Bharat HD", "Sony TV", "Sony TV HD", "Sony SAB", "Sony TV HD (1)", "Sony TV HD (2)",
-        "Sony SAB HD", "Sony sab HD", "Colors TV", "Colors TV HD","IN | Zee TV", "Zee TV HD", "Zee Anmol", "Sony Pal", "IN | & TV", 
-        "& TV HD", "IN | STAR PLUS", "STAR PLUS HD", "STAR BHARAT", "STAR BHARAT HD", "SONY TV", "SONY TV HD", "COLORS (FHD)", "Star Utsav",
-        "IN | SONY SAB", "SONY SAB HD", "COLORS TV", "COLORS TV HD","ZEE TV", "ZEE TV HD", "ZEE ANMOL", "SONY PAL", "SONY HD", "COLOR HD", "COLORS FHD", "IN | SONY SET HD"
+        "SONY SD", "Sony SAB", "Sony TV HD (1)", "Sony TV HD (2)",
+        "Sony SAB HD", "Sony sab HD", "Colors TV", "Colors TV HD","IN | Zee TV", "Zee TV HD", "Zee Anmol", "Sony Pal", "& TV", 
+        "& TV HD", "STAR PLUS", "STAR PLUS HD", "STAR BHARAT", "STAR BHARAT HD", "SONY", "SONY HD", "COLORS HD", "Star Utsav",
+        "SONY SAB", "SONY SAB HD", "COLORS TV", "COLORS TV HD","ZEE TV", "ZEE TV HD", "ZEE ANMOL", "SONY PAL", "SONY HD", "COLOR HD", "COLORS FHD", "IN | SONY SET HD"
  ],
     "Movies": [
         "Star Gold", "Star Gold HD", "Star Gold Select", "Star Gold Select HD", "Zee Cinema", "Zee Cinema HD", "Zee Action", "Zee Bollywood", "Zee Classic",
